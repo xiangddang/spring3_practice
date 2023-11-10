a3f7e967eb23dc61b5666771e84f8649745d8197
Frances <xiang.yany@northeastern.edu>
2023-11-10 17:17:31 -0500

    html table

--- backend/spring-boot-ecommerce/src/main/java/com/frances/ecommerce/dao/ProductRepository.java
+++ backend/spring-boot-ecommerce/src/main/java/com/frances/ecommerce/dao/ProductRepository.java
@@ -4,7 +4,9 @@ package com.frances.ecommerce.dao;
 import com.frances.ecommerce.entity.Product;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
 }
